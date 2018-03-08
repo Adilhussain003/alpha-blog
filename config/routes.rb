934ada4cad0685c7bc1586c17e6841e75a3b500d
Adil Hussain <adilhussain003@gmail.com>
2018-03-08 17:35:57 +0500

    Create show action and add gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  get 'signup', to:'users#new'
 
+  resources :users, execpt: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
